637f7fd28b96ef0c3fb961135b155647b965b9aa
Michael Pope <amorphid@captaincoder.io>
2014-03-02 17:32:49 -0800

    Create initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootyCall::Application.config.secret_key_base = '6ea8130816f795b492b9fed09d396e2dd95b00671b1a134fbbc97231d41f84a7a364d1cda60824c6be6c33a111a1fcd8ba0854825ee5f1fdf583487a3ee99db4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BootyCall::Application.config.session_store :cookie_store, key: '_booty_call_session'
