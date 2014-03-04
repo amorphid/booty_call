897d915a92f64e6b4bb10eba8436ef8d4a0f9f9c
Michael Pope <amorphid@captaincoder.io>
2014-03-04 11:29:51 -0800

    (red) Write 4 failing model tests => BootyAddress validations

--- spec/models/booty_address_spec.rb
+++ spec/models/booty_address_spec.rb
@@ -1,4 +1,8 @@
 require "spec_helper"
 
 describe BootyAddress do
+  it { should validate_presence_of(:street1) }
+  it { should validate_presence_of(:city)    }
+  it { should validate_presence_of(:state)   }
+  it { should validate_presence_of(:zip)     }
 end
