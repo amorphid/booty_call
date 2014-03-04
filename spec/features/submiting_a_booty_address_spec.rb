9ffb134df730846ea7cf0be4dc696c84113bc548
Michael Pope <amorphid@captaincoder.io>
2014-03-04 12:37:43 -0800

    (red) Write failing feature test => submitting a booty address

--- spec/features/submiting_a_booty_address_spec.rb
+++ spec/features/submiting_a_booty_address_spec.rb
@@ -3,7 +3,16 @@ require "spec_helper"
 feature "Submitting a booty address" do
   background { visit new_booty_address_path }
 
-  scenario "displays success message if input valid"
+  scenario "displays success message if input valid" do
+    b = Fabricate.attributes_for(:booty_address)
+    fill_in :booty_address_street1,    with: b[:street1]
+    fill_in :booty_address_apt_number, with: b[:apt_number]
+    fill_in :booty_address_city,       with: b[:city]
+    fill_in :booty_address_state,      with: b[:state]
+    fill_in :booty_address_zip,        with: b[:zip]
+    click_button("Submit")
+    expect(page.body).to have_content("Booty Address was successfully created")
+  end
 
   scenario "displays failure message if input invalid" do
     click_button("Submit")
