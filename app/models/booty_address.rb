9943031dac74576e67e08c5dd19b78bbae69b919
Michael Pope <amorphid@captaincoder.io>
2014-03-04 11:35:02 -0800

    (green) Make 4 model tests pass => BootyAddress validations

--- app/models/booty_address.rb
+++ app/models/booty_address.rb
@@ -1,2 +1,13 @@
 class BootyAddress < ActiveRecord::Base
+  validates :city,
+            presence: true
+
+  validates :state,
+            presence: true
+
+  validates :street1,
+            presence: true
+
+  validates :zip,
+            presence: true
 end
