8cb84b9b53579ec8bdccf9859fb9e84ba04a93d3
Michael Pope <amorphid@captaincoder.io>
2014-03-04 12:07:56 -0800

    (green) Make controller test pass => booty_addresses#create

--- app/controllers/booty_addresses_controller.rb
+++ app/controllers/booty_addresses_controller.rb
@@ -1,11 +1,23 @@
 class BootyAddressesController < ApplicationController
   def create
-    @booty_address = BootyAddress.new
+    @booty_address = BootyAddress.new(params_booty_address)
     @booty_address.save
     render action: "new"
   end
 
   def new
     @booty_address = BootyAddress.new
+  end
+
+  private
+
+  def params_booty_address
+    params[:booty_address].permit(
+      :apt_number,
+      :city,
+      :state,
+      :street1,
+      :zip
+    )
   end
 end
