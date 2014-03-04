d403e8610be97fbacc31801153dd4e2bed7b79f9
Michael Pope <amorphid@captaincoder.io>
2014-03-04 12:50:26 -0800

    (green) Make feature test pass => submitting a booty address

--- app/controllers/booty_addresses_controller.rb
+++ app/controllers/booty_addresses_controller.rb
@@ -3,6 +3,7 @@ class BootyAddressesController < ApplicationController
     @booty_address = BootyAddress.new(params_booty_address)
 
     if @booty_address.save
+      flash[:notice] = "Booty Address was successfully created"
       redirect_to booty_address_path(@booty_address)
     else
       render action: "new"
