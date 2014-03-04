ee3e31bc8a8d8a5656af1660a5e8290b0299d7b7
Michael Pope <amorphid@captaincoder.io>
2014-03-03 22:07:07 -0800

    Write 4 pending controller tests => booty_addresses#create

--- spec/controllers/booty_addresses_controller_spec.rb
+++ spec/controllers/booty_addresses_controller_spec.rb
@@ -1,10 +1,17 @@
 require "spec_helper"
 
 describe BootyAddressesController do
-  context "GET /new" do
+  context "#new" do
     it "sets @booty_address" do
       get :new
       expect(assigns[:booty_address]).to be_instance_of(BootyAddress)
     end
+  end
+
+  context "#create" do
+    it "sets @booty_address"
+    it "redirects to #show"
+    it "creates booty with valid input"
+    it "does not create boot with invalid input"
   end
 end
