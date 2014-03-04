7d10efc24a786a9c7ad371a045c5cfa5d4afe186
Michael Pope <amorphid@captaincoder.io>
2014-03-03 18:38:53 -0800

    (red) Write failing feature test => submitting a booty address

--- spec/features/submiting_a_booty_address_spec.rb
+++ spec/features/submiting_a_booty_address_spec.rb
@@ -1,6 +1,16 @@
 require "spec_helper"
 
 feature "Submitting a booty address" do
+  background { visit new_booty_address_path }
+
   scenario "displays success message if input valid"
-  scenario "displays failure message if input invalid"
+
+  scenario "displays failure message if input invalid" do
+    click_button("Submit")
+    expect(page.body).to have_content("street1 can't be blank")
+    expect(page.body).to have_content("apt_numer can't be blank")
+    expect(page.body).to have_content("city  can't be blank")
+    expect(page.body).to have_content("state can't be blank")
+    expect(page.body).to have_content("zip can't be blank")
+  end
 end
