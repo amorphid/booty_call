c09e138e7b6eececbc6353c273a2f34ac9418ec5
Michael Pope <amorphid@captaincoder.io>
2014-03-03 22:17:41 -0800

    (green) Make controller test pass => booty_addresses#create

--- app/controllers/booty_addresses_controller.rb
+++ app/controllers/booty_addresses_controller.rb
@@ -1,4 +1,9 @@
 class BootyAddressesController < ApplicationController
+  def create
+    @booty_address = BootyAddress.new
+    render action: "new"
+  end
+
   def new
     @booty_address = BootyAddress.new
   end
